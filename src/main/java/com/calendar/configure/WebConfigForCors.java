0047532b6b2e5a884ea4f2a30c0b8e60a54d9a2b
ahilendra9singh8 <shailendra9.singh8@gmail.com>
2025-03-17 18:24:01 +0530

    revert changes

--- src/main/java/com/calendar/configure/WebConfigForCors.java
+++ src/main/java/com/calendar/configure/WebConfigForCors.java
@@ -4,27 +4,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class WebConfigForCors implements WebMvcConfigurer {
-//    
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*");
-//    }
-//}
-
 @Configuration
 public class WebConfigForCors implements WebMvcConfigurer {
     
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://13.201.83.130:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true); 
+                .allowedHeaders("*");
     }
 }
+
+//@Configuration
+//public class WebConfigForCors implements WebMvcConfigurer {
+//    
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://13.201.83.130:3000")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true); 
+//    }
+//}
